51b7c8cafe2809e539290c8673d1ccd6c8aaa314
m-hilgendorf <mike@hilgendorf.audio>
2024-05-08 09:26:29 -0500

    feat: add constructor helpers for utility messages

--- src/message/utility.rs
+++ src/message/utility.rs
@@ -2,12 +2,12 @@
 use core::ops::Deref;
 
 use crate::message::Message;
-use crate::packet::{MessageType, Packet32};
+use crate::packet::{MessageType, Packet, Packet32};
 
 /// Utility messages defined by the MIDI 2.0 specification, including
 /// jitter reduction and no-op.
 #[derive(Copy, Clone, Hash, Debug, Eq, PartialEq)]
-pub struct Utility(Packet32);
+pub struct Utility(pub(crate) Packet32);
 
 /// Represents the status byte of a [Utility] message.
 #[derive(Copy, Clone, Hash, Debug, Eq, PartialEq)]
@@ -32,6 +32,26 @@ pub enum UtilityStatus {
 impl Utility {
     pub(crate) fn from_packet_unchecked(ump: Packet32) -> Self {
         Self(ump)
+    }
+
+    /// Create a new jitter reduction timestamp message.
+    pub fn jr_timestamp(timestamp: u16) -> Self {
+        Self(Packet([0x0020_0000 | (timestamp as u32)]))
+    }
+
+    /// Create a new jitter reduction clock message.
+    pub fn jr_clock(clock: u16) -> Self {
+        Self::from_packet_unchecked(Packet([0x0020_0000 | (clock as u32)]))
+    }
+
+    /// Create a new delta clockstamp message.
+    pub fn delta_clockstamp(clock: u16) -> Self {
+        Self::from_packet_unchecked(Packet([0x0020_0000 | (clock as u32)]))
+    }
+
+    /// Create a new delta clockstamp message in ticks per quarternote.
+    pub fn delta_clockstamp_ticks_per_quarternote(clock: u16) -> Self {
+        Self::from_packet_unchecked(Packet([0x0020_0000 | (clock as u32)]))
     }
 }
 
